a4839a9681055feeffdb6c0adeb61cc308d21b4a
Phelipy Julião <124466082+phelipywithph@users.noreply.github.com>
2025-11-20 19:57:38 +0000

    Autaracao: atualização da classe departamento. Da linha 33 à linha 82

--- models/departamento.py
+++ models/departamento.py
@@ -30,3 +30,53 @@ class Departamento:
             data.get('created_at'),
             data.get('updated_at')
         )
+    def __str__(self) -> str:
+        return (
+            f'Departamento(numero_departamento={self._numero_departamento}'
+            f'data_de_abertura={self._data_de_abertura}, endereco={self._endereco},'
+            f'numero_departamento={self._numero_departamento}, '
+            f'created_at={self._created_at}, updated_at={self._updated_at})'
+        )
+    @property
+    def endereco(self) -> str:
+        return self._endereco
+
+    @endereco.setter
+    def endereco(self, endereco: str):
+        self._endereco = endereco
+        self._updated_at = datetime.now()
+
+    @property
+    def sexo(self) -> str:
+        return self._sexo
+
+    @sexo.setter
+    def sexo(self, sexo: str):
+        self._sexo = sexo
+        self._updated_at = datetime.now()
+
+    @property
+    def cpf_supervisor(self) -> Optional[str]:
+        return self._cpf_supervisor
+
+    @cpf_supervisor.setter
+    def cpf_supervisor(self, cpf_supervisor: Optional[str]):
+        self._cpf_supervisor = cpf_supervisor
+        self._updated_at = datetime.now()
+
+    @property
+    def numero_departamento(self) -> Optional[int]:
+        return self._numero_departamento
+
+    @numero_departamento.setter
+    def numero_departamento(self, numero_departamento: Optional[int]):
+        self._numero_departamento = numero_departamento
+        self._updated_at = datetime.now()
+
+    @property
+    def created_at(self) -> Optional[datetime]:
+        return self._created_at
+
+    @property
+    def updated_at(self) -> Optional[datetime]:
+        return self._updated_at
